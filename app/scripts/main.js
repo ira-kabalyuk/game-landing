75aad1112d34b4651198758bd436e14167ebcf7a
ira-kabalyuk <jokky@bigmir.net>
2017-08-04 13:26:31 +0300

    dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,24 +21,54 @@ jQuery(document).ready(function() {
 
 	
   // variables  
-	
-(function($) {
-  
+	var pageVisited = false;
+
+(function($) {  
   'use strict';
 
   // initialize fullPage
+	
+
   $('#fullpage').fullpage({		
 		anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixSection', 'sevenSection'],
 		// scrollOverflow: true,
 		// slidesNavigation: true,
     navigation: true,
+		
     onLeave: function(index, nextIndex, direction) {
 			if(nextIndex == 3) {
-				$('.count-number').countTo({       
+				/*$('.count-number').countTo({       
         speed: 3000       
-    });
+    });*/
 
-				/*$('#playBtn').trigger('click');*/
+				/*$('#playBtn_1').trigger('click');
+				$('#playBtn_2').trigger('click');
+				$('#playBtn_3').trigger('click');
+				$('#playBtn_4').trigger('click');*/
+				
+				if(!pageVisited) {				
+
+				var spinTimePeriod = 1000;
+				var spinTime = 0;
+				var spinners = $('.playBtn');
+
+				var counters = $('.count-number');
+				spinners.each(function(i) {
+					var spinner = $(spinners[i]);
+					var counter = $(counters[i]);
+					
+					setTimeout(function(){
+						spinner.trigger('click');
+						counter.countTo({
+							speed: spinTimePeriod						
+						})
+					}, spinTime);
+
+					spinTime = spinTime + spinTimePeriod;
+				});
+
+					pageVisited = true;
+				}
 			}			
       
       if(nextIndex == 2 && $(document).width() > 1025) { 
@@ -47,7 +77,7 @@ jQuery(document).ready(function() {
       }	
 
 			if(nextIndex == 3 && $(document).width() > 1025) {			
-				$('.zoom-3').css({'animation-delay': '0.5s', 'animation-duration': '1s'}).addClass('animated zoomIn');				
+				$('.zoom-3').css({'animation-delay': '4s', 'animation-duration': '1s'}).addClass('animated zoomIn');				
       }	
 
 			if(nextIndex == 4 && $(document).width() > 1025) {			
@@ -55,9 +85,10 @@ jQuery(document).ready(function() {
 				$('.zoom-4').css({'animation-delay': '1s', 'animation-duration': '1.5s'}).addClass('animated zoomIn');
       }
 
-			if(nextIndex == 5 && $(document).width() > 1025) {			
-				$('.deg').css({'animation-delay': '1s', 'animation-duration': '2s'}).addClass('animated flip');
-				$('.post').css({'animation-delay': '1s', 'animation-duration': '1s'}).addClass('animated fadeIn');
+			if(nextIndex == 5 && $(document).width() > 1025) {				
+				$('.post').css({'animation-delay': '1s', 'animation-duration': '2s'}).addClass('animated fadeIn');
+				$('.show-5').css({'animation-delay': '0.5s', 'animation-duration': '1s'}).addClass('animated bounceIn');
+				
       }	
 
 				if(nextIndex == 6 && $(document).width() > 1025) {			
@@ -65,9 +96,9 @@ jQuery(document).ready(function() {
 				$('.zoom-6').css({'animation-delay': '1s', 'animation-duration': '1.5s'}).addClass('animated zoomIn');
       }	
 
-				if(nextIndex == 7 && $(document).width() > 1025) {			
-				$('.down').css({'animation-delay': '2s', 'animation-duration': '2s'}).addClass('animated slideInDown');
-				$('.popup-link').css({'animation-delay': '1s', 'animation-duration': '1s'}).addClass('animated bounceIn');
+				if(nextIndex == 7 && $(document).width() > 1025) {				
+				$('.down').css({'animation-delay': '1s', 'animation-duration': '1s'}).addClass('animated slideInDown');
+				$('.popup-link').css({'animation-delay': '0.2s', 'animation-duration': '1s'}).addClass('animated bounceIn');
       }
 		
     }
@@ -168,7 +199,7 @@ jQuery(document).ready(function() {
 
 	  $('.slot-1').jSlots({  
         spinner : '#playBtn_1',
-				time : 4000,
+				time : 1500,
         winnerNumber : 3,
 				loops : 8,
 				easing : 'linear' 
@@ -176,7 +207,7 @@ jQuery(document).ready(function() {
 
 		 $('.slot-2').jSlots({  
         spinner : '#playBtn_2',
-				time : 4000,
+				time : 1500,
         winnerNumber : 3,
 				loops : 8,
 				easing : 'linear' 
@@ -184,7 +215,7 @@ jQuery(document).ready(function() {
 
 		 $('.slot-3').jSlots({  
         spinner : '#playBtn_3',
-				time : 4000,
+				time : 1500,
         winnerNumber : 3,
 				loops : 8,
 				easing : 'linear' 
@@ -192,27 +223,15 @@ jQuery(document).ready(function() {
 
 		 $('.slot-4').jSlots({  
         spinner : '#playBtn_4',
-				time : 4000,
+				time : 1500,
         winnerNumber : 3,
 				loops : 8,
 				easing : 'linear' 
-    });    
-
-/*	$.jSlots.defaultOptions = {  
-    number : 4,          // Number: number of slots  
-    winnerNumber : 1,    // Number: list item number upon which to trigger a win, 1-based index, NOT ZERO-BASED  
-    spinner : '',        // CSS Selector: element to bind the start event to  
-    spinEvent : 'click', // String: event to start slots on this event  
-    onStart : $.noop,    // Function: runs on spin start,  
-    onEnd : $.noop,      // Function: run on spin end. It is passed (finalNumbers:Array). finalNumbers gives the index of the li each slot stopped on in order.  
-    onWin : $.noop,      // Function: run on winning number. It is passed (winCount:Number, winners:Array, finalNumbers:Array)  
-    easing : 'swing',    // String: easing type for final spin. I recommend the easing plugin and easeOutSine, or an easeOut of your choice.  
-    time : 7000,         // Number: total time of spin animation  
-    loops : 6            // Number: times it will spin during the animation
-};  */
-
+    });
 
 });
+
+
 
 
 
